fc1e9f147c5b12d294ff118c9cf6f0dd05b5f193
justpiyoosh <justpiyoosh@gmail.com>
2021-04-14 21:02:51 +0530

    Create read view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def show
+        @user = User.find(params[:id])
+      end
+      
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def delete
+
+    end
+
+end
