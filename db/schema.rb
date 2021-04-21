0fc92ad8e43593f57c5fa1cf80247852894c9d00
psmindmatch <ps@mindmatch.ai>
2021-04-21 21:22:33 +0530

    Add phone_no in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_100139) do
+ActiveRecord::Schema.define(version: 2021_04_21_154933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_100139) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "phone_no"
   end
 
 end
