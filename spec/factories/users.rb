3405673a557c170a67d766fa3e0a8e113728314e
psmindmatch <ps@mindmatch.ai>
2021-04-23 20:22:16 +0530

    Indentation fix

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,11 @@
 FactoryBot.define do
-    factory :user do
-         username {"justpiyoosh07"}
-         email    {"justpiyoosh07@gmail.com"}
-    end
+  factory :user do
+    username {"justpiyoosh07"}
+    email    {"justpiyoosh07@gmail.com"}
+  end
 
-
-    factory :random_user, class: User do
-        username {Faker::Name.username}
-        email    {Faker::Internet.safe_email}
-   end
+  factory :random_user, class: User do
+    username {Faker::Name.username}
+    email    {Faker::Internet.safe_email}
+  end
 end
