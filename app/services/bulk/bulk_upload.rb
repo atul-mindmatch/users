6e3035067aab56e711bc1012c9e3b88d1117f5f0
psmindmatch <ps@mindmatch.ai>
2021-05-06 14:00:25 +0530

    Add sequential search for user validation

--- app/services/bulk/bulk_upload.rb
+++ app/services/bulk/bulk_upload.rb
@@ -15,6 +15,7 @@ module Bulk
       batch_size = 1000
       
       while x < (table.size) 
+        users = []
         all_users = []
         all_user_details = []
         user_details = []
@@ -26,35 +27,53 @@ module Bulk
           dob = table.by_row[x]["dob"]
           primary_address = table.by_row[x]["primary_address"]
           secondary_address = table.by_row[x]["secondary_address"]
-          user = { "email" => ActiveRecord::Base.connection.quote(email) , "username" => ActiveRecord::Base.connection.quote(username) }
-          user_detail = { 
+          found1 = false
+          found2 = false
+          for user in users do
+            if user["email"] == ActiveRecord::Base.connection.quote(email)
+              found1 = true
+            end
+            if user["username"] == ActiveRecord::Base.connection.quote(username)
+              found2 = true
+            end
+          end
+          if (User.find_by(email: email) == nil and User.find_by(username: username) == nil and !found1 and !found2)
+            user = { "email" => ActiveRecord::Base.connection.quote(email) , "username" => ActiveRecord::Base.connection.quote(username) }
+            user_detail = { 
             "first_name" => ActiveRecord::Base.connection.quote(first_name), 
             "last_name" => ActiveRecord::Base.connection.quote(last_name),
             "dob" => ActiveRecord::Base.connection.quote(dob),
             "user_id" => nil,
             "primary_address" => ActiveRecord::Base.connection.quote(primary_address),
             "secondary_address" => ActiveRecord::Base.connection.quote(secondary_address) 
-          }
-          all_users.push(<<-SQL.chomp)
-            (
-              #{user.values.join(',')},
-              NOW(),
-              NOW()
-            )
-          SQL
+            } 
+            users.push(user)
+            all_user_details.push(user_detail)
+          end
           x = x + 1;
-          all_user_details.push(user_detail)
           if(x >= table.size) 
             break
           end
         end
         if(x <= table.size)
-          sql = <<-SQL.chomp
-            INSERT INTO users (
-            email, username, created_at, updated_at
-            ) VALUES #{all_users.join(',')}
+          for user in users
+            all_users.push(<<-SQL.chomp)
+            (
+              #{user.values.join(',')},
+              NOW(),
+              NOW()
+            )
             SQL
-          user_ids = ActiveRecord::Base.connection.execute(sql+'returning id')
+          end
+
+          if(all_users.size != 0)
+            sql = <<-SQL.chomp
+            INSERT INTO users (
+              email, username, created_at, updated_at
+              ) VALUES #{all_users.join(',')}
+              SQL
+            user_ids = ActiveRecord::Base.connection.execute(sql+'returning id')
+          end
         end
         temp = 0    
         for user_detail in all_user_details
@@ -62,22 +81,21 @@ module Bulk
           temp = temp + 1;
         end
 
-        for user_detail in all_user_details
-          user_details.push(<<-SQL.chomp)
-          (
-            #{user_detail.values.join(',')},
-            NOW(),
-            NOW()
-          )
-          SQL
-        end
-
-        if(x <= table.size)
+        if(all_users.size != 0)
+          for user_detail in all_user_details
+            user_details.push(<<-SQL.chomp)
+            (
+              #{user_detail.values.join(',')},
+              NOW(),
+              NOW()
+            )
+            SQL
+          end
           sql = <<-SQL.chomp
             INSERT INTO user_details (
               first_name , last_name , dob, user_id ,primary_address , secondary_address , created_at, updated_at
-                ) VALUES #{user_details.join(',')}
-              SQL
+              ) VALUES #{user_details.join(',')}
+            SQL
 
           ActiveRecord::Base.connection.execute(sql)
         end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_103011) do
+ActiveRecord::Schema.define(version: 2021_05_03_133032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 2021_04_24_103011) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
